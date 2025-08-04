3be819185786fc01c5dcbe41b136aab48843c3cb
Clovis Hatungimana <hatungclovis@gmail.com>
2025-08-03 22:48:34 -0400

    Downgrade: Expo SDK 51 for Kotlin compatibility

--- metro.config.js
+++ metro.config.js
@@ -2,20 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Optimize bundle to reduce warnings
+// Optimize for Expo SDK 51
 config.resolver.platforms = ['native', 'android', 'ios', 'web'];
-
-// Reduce bundle warnings
-config.transformer.minifierConfig = {
-  mangle: {
-    keep_fnames: true,
-  },
-  output: {
-    comments: false,
-  },
-};
-
-// Enable Hermes for better performance
-config.transformer.hermesParser = true;
 
 module.exports = config;
