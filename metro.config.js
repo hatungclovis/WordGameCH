c763a94d654e76e033c4dd095fab6feb5732ada5
Clovis Hatungimana <clovis.hatungimana@emcitv.com>
2025-08-04 00:39:37 -0400

    Fix yaml file for codemagic

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Optimize for Expo SDK 51
-config.resolver.platforms = ['native', 'android', 'ios', 'web'];
-
 module.exports = config;
